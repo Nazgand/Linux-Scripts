324fda1297b040d9b7e0009971eb9ba289a208cc
Mark Andrew Gerads <nazgand@gmail.com>
2019-11-14 18:17:41 -0600

    XFCE instead of Gnome

--- autoSAI.sh
+++ autoSAI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#At startup, contribute GPU to the Sensible Artificial Intelligence for playing Go http://sai.unich.it/
+#At startup, contribute GPU to http://sai.unich.it/
 cd ~/git/sai/autogtp/
-gnome-terminal -- bash -c "./autogtp --url http://sai.unich.it/ --username nazgand --password censored -g 2; exec bash"
+xfce4-terminal --title='Sai AutoGTP' -e 'bash -c "./autogtp --url http://sai.unich.it/ --username nazgand --password censored -g 2; bash"'
 
